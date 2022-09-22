19d7cfd68139a0573fe67ef5cceae5044de9e3dc
LionetChen <chinalionet@gmail.com>
2022-09-22 16:09:26 +1000

    Add MementoPattern

---
+++ src/MementoPattern/Caretaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern;
+public class Caretaker
+{
+    Stack<IMementoMeta> _roleStateMementos = new Stack<IMementoMeta>();
+
+    public void Save(IMementoMeta memo)
+    {
+        _roleStateMementos.Push(memo);
+    }
+    
+    public IMementoMeta RestoreLast()
+    {
+        return _roleStateMementos.Pop();
+    }
+}
---
+++ src/MementoPattern/GameRoleMemento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoPattern;
+public class GameRoleMemento : IMementoMeta
+{
+    public GameRoleMemento(string tag, int hitPoitns, int manaPoints, int damage)
+    {
+        HitPoitns = hitPoitns;
+        ManaPoints = manaPoints;
+        Damage = damage;
+        Tag = tag;
+        CreateTime = DateTime.Now;
+    }
+
+    public int HitPoitns { get; set; }
+    public int ManaPoints { get; set; }
+    public int Damage { get; set; }
+
+    public string Tag { get; set; }
+
+    public DateTime CreateTime { get; set; }
+}
