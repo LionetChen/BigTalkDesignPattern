024e32292a827ef40ba3a1c3152410cfb2850172
LionetChen <chinalionet@gmail.com>
2022-09-24 22:58:26 +1000

    Add FlyweightPattern project with original implemtation before flyweight

---
+++ src/BigTalkDesignPattern.Tests/FlyweightPatternTests.cs
@@ -0,0 +1,36 @@
+﻿using FlyweightPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Diagnostics;
+
+namespace BigTalkDesignPattern.Tests;
+[TestClass]
+public class FlyweightPatternTests
+{
+    [TestMethod]
+    public void MyTestMethod()
+    {
+        using (Process proc = Process.GetCurrentProcess())
+        {
+            proc.Refresh();
+            Console.WriteLine($"{proc.ProcessName} {proc.PrivateMemorySize64 / 1024 / 1024} MB");
+
+            List<IDrawTree> listOfTrees = new List<IDrawTree>();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                OriginalTree tree = new OriginalTree(1 + i / 1000);
+                listOfTrees.Add(tree);
+            }
+
+            proc.Refresh();
+            Console.WriteLine($"{proc.ProcessName} {proc.PrivateMemorySize64 / 1024 / 1024} MB");
+
+            for (int i = 0; i < listOfTrees.Count; i++)
+            {
+                Assert.AreEqual($"Tree the size of {1 + (i / 1000)}, image has {1024 * 10} bytes", listOfTrees[i].Draw());
+            }
+        }
+    }
+}
---
+++ src/FlyweightPattern/OriginalTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightPattern;
+public class OriginalTree : ITree, IDrawTree
+{
+    public OriginalTree(int size)
+    {
+        Size = size;
+        Sprite = new byte[1024 * 10];
+    }
+
+    public byte[] Sprite { get; private set; }
+
+    public int Size { get; private set; }
+
+    public string Draw()
+    {
+        return $"Tree the size of {Size}, image has {Sprite.Length} bytes";
+    }
+}
